8c31c6fe649f7bafc516a5081837dc6eff66ae0a
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-10-02 14:39:47 +0700

    feat : add restaurant data

---
+++ lib/screens/list_restaurant.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/resources/colors.dart';
+
+class ListRestaurant extends StatefulWidget {
+  const ListRestaurant({super.key});
+
+  @override
+  State<ListRestaurant> createState() => _ListRestaurantState();
+}
+
+class _ListRestaurantState extends State<ListRestaurant> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 150,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 120,
+                      padding: EdgeInsets.all(24),
+                      color: RestaurantAppColors.PRIMARY_COLOR,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            'Hola...',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Already hungry?",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Card(
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: InkWell(
+                              onTap: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 16),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Find Restaurant",
+                                      style: TextStyle(
+                                          color:
+                                              RestaurantAppColors.GREY_COLOR_1),
+                                    ),
+                                    Icon(
+                                      Icons.search,
+                                      color: RestaurantAppColors.PRIMARY_COLOR,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
