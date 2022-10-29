3642084855169c38fcbc96c64f2a5ea4e08e3259
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-10-30 00:03:07 +0700

    feat : add api test

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter_application_1/data/responses/restaurant_add_review_response.dart';
 import 'package:flutter_application_1/data/responses/restaurant_detail_response.dart';
 import 'package:flutter_application_1/data/responses/restaurant_list_response.dart';
 import 'package:flutter_application_1/data/responses/restaurant_search_response.dart';
 import 'package:flutter_application_1/services/endpoint.dart';
-import 'package:flutter_application_1/data/models/restaurant.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -15,8 +13,7 @@ class ApiService {
 
   Future<RestaurantListResponse> getListRestaurants() async {
     try {
-      final response =
-          await http.get(Uri.parse("${_baseUrl}${Endpoints.restaurantList}"));
+      final response = await http.get(Uri.parse("${_baseUrl}${Endpoints.restaurantList}"));
       if (response.statusCode == 200) {
         return RestaurantListResponse.fromJson(jsonDecode(response.body));
       } else {
@@ -30,8 +27,7 @@ class ApiService {
 
   Future<RestaurantDetailResponse> getDetailRestaurant(String id) async {
     try {
-      final response = await http
-          .get(Uri.parse("${_baseUrl}${Endpoints.restaurantDetail}${id}"));
+      final response = await http.get(Uri.parse("${_baseUrl}${Endpoints.restaurantDetail}${id}"));
       if (response.statusCode == 200) {
         return RestaurantDetailResponse.fromJson(jsonDecode(response.body));
       } else {
@@ -44,8 +40,7 @@ class ApiService {
   }
 
   Future<RestaurantSearchResponse> searchRestaurant(String query) async {
-    final response = await http
-        .get(Uri.parse("${_baseUrl}${Endpoints.restaurantSearch}?q=${query}"));
+    final response = await http.get(Uri.parse("${_baseUrl}${Endpoints.restaurantSearch}?q=${query}"));
     if (response.statusCode == 200) {
       return RestaurantSearchResponse.fromJson(json.decode(response.body));
     } else {
@@ -53,8 +48,7 @@ class ApiService {
     }
   }
 
-  Future<RestaurantAddReviewResponse> postReviews(
-      String id, String name, String review) async {
+  Future<RestaurantAddReviewResponse> postReviews(String id, String name, String review) async {
     final response = await http.post(
       Uri.parse("${_baseUrl}${Endpoints.review}"),
       headers: {
---
+++ test/api/api_service_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_application_1/data/responses/restaurant_detail_response.dart';
+import 'package:flutter_application_1/data/responses/restaurant_list_response.dart';
+import 'package:flutter_application_1/services/api_service.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'api_service_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  group('Cek Api Resto', () {
+    test('Return List Resto', () async {
+      final client = MockClient();
+      when(client.get(Uri.parse('https://restaurant-api.dicoding.dev/list'))).thenAnswer(
+          (_) async => http.Response('{"error":false,"message":"success","count":20,"restaurants":[]}', 200));
+
+      expect(await ApiService().getListRestaurants(), isA<RestaurantListResponse>());
+    });
+    test('Return Detail Resto', () async {
+      final client = MockClient();
+      when(client.get(Uri.parse('https://restaurant-api.dicoding.dev/detail/rqdv5juczeskfw1e867')))
+          .thenAnswer((_) async => http.Response('{"error":false,"message":"success","restaurant":{}}', 200));
+
+      expect(await ApiService().getDetailRestaurant("rqdv5juczeskfw1e867"), isA<RestaurantDetailResponse>());
+    });
+  });
+}
